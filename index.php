cb4be25675d5a67adeea980ce7d4efa2f447164c
Purag Moumdjian <purmou@gmail.com>
2014-11-16 02:54:42 -0600

    Create style.css

--- index.php
+++ index.php
@@ -7,44 +7,18 @@
   <link href="http://fonts.googleapis.com/css?family=Titillium+Web:400,700,900"
         rel="stylesheet">
         
+  <link href="style.css"
+        rel="stylesheet">
+        
   <script src="//code.jquery.com/jquery-latest.min.js"></script>
   <script src="jquery.fittext.js"></script>
+  
   <script>
     $(function(){
       $("h1").fitText(0.6);
     });
   </script>
   
-  <style>
-  
-    body {
-      background: #c40000;
-      margin: 0;
-      padding: 0;
-    }
-    
-    header {
-      height:10%;
-    }
-    
-    h1 {
-      width:100%;
-      height:100%;
-      line-height:100%;
-      
-      text-align: center;
-      margin: 0;
-      padding: 0;
-      font-family: 'Titillium Web', sans-serif;
-      font-weight: 900;
-      color: #880000;
-      
-      background: -webkit-linear-gradient( #880000 0%, rgba(0,0,0,0) 90% );
-      -webkit-background-clip: text;
-      -webkit-text-fill-color:transparent;
-    }
-  
-  </style>
 </head>
 <body>
   <main>
