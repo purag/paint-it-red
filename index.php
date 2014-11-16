d0cec9b0c011072dbf2b02fdc5c0b687b10071b7
Purag Moumdjian <purmou@gmail.com>
2014-11-16 02:46:01 -0600

    Gradient text coloring + fitText applied to header

--- index.php
+++ index.php
@@ -4,7 +4,16 @@
   <meta charset="utf-8">
   <title>Paint it Red</title>
   
-  <link href="http://fonts.googleapis.com/css?family=Titillium+Web:400,700,900" rel="stylesheet">
+  <link href="http://fonts.googleapis.com/css?family=Titillium+Web:400,700,900"
+        rel="stylesheet">
+        
+  <script src="//code.jquery.com/jquery-latest.min.js"></script>
+  <script src="jquery.fittext.js"></script>
+  <script>
+    $(function(){
+      $("h1").fitText(0.6);
+    });
+  </script>
   
   <style>
   
@@ -14,15 +23,25 @@
       padding: 0;
     }
     
+    header {
+      height:10%;
+    }
+    
     h1 {
+      width:100%;
+      height:100%;
+      line-height:100%;
+      
       text-align: center;
       margin: 0;
       padding: 0;
-      line-height:180px;
       font-family: 'Titillium Web', sans-serif;
-      font-size: 14em;
       font-weight: 900;
-      color: #990000;
+      color: #880000;
+      
+      background: -webkit-linear-gradient( #880000 0%, rgba(0,0,0,0) 90% );
+      -webkit-background-clip: text;
+      -webkit-text-fill-color:transparent;
     }
   
   </style>
