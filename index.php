b44a896f44502225c7dfaf0372eb4c431367c8eb
Purag Moumdjian <pmoumdji@eng.ucsd.edu>
2014-11-16 01:41:44 -0600

    Working on index

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Paint it Red</title>
+</head>
+<body>
+  <main>
+    <header>
+      <h1>
+        PAINT IT RED
+      </h1>
+    </header>
+    
+    <article>
+      Just some content -- for now!
+    </article>
+  </main>
+</body>
